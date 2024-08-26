0b9d5bb7ecf945455c2fc20b266058c79b1e432b
Omkar Ambi <omkarambi44@gmail.com>
2024-08-26 23:36:42 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'core',
+    loadChildren: () => import('./core/core.module').then(m => m.CoreModule)
+  },
+  {
+    path: 'shared',
+    loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule)
+  },
+  {
+    path: 'component',
+    loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
